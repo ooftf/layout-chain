d8b20ad7888e42fb17c84357fb0f7b17b59bf3e8
ooftf <li994749769@gmail.com>
2021-07-09 18:32:49 +0800

    Initial commit

---
+++ app/src/main/java/com/ooftf/demo/layout_chain/S.java
@@ -0,0 +1,15 @@
+package com.ooftf.demo.layout_chain;
+
+import android.annotation.Nullable;
+
+/**
+ * @author ooftf
+ * @email 994749769@qq.com
+ * @date 2021/7/9
+ */
+class S {
+    @Nullable
+    String text() {
+        return "";
+    }
+}
