630ca991e8f314b9df5461db62364b424798af21
ooftf <994749769@qq.com>
2021-07-11 23:40:25 +0800

    sync

--- app/src/main/java/com/ooftf/demo/layout_chain/MainActivity.kt
+++ app/src/main/java/com/ooftf/demo/layout_chain/MainActivity.kt
@@ -3,13 +3,14 @@ package com.ooftf.demo.layout_chain
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ooftf.demo.layout_chain.demo0.Demo0Activity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, Demo0Activity::class.java))
     }
-
 
 
 }
